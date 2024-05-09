1e68006a9c79e268b262c19dd27812a08916b76b
Subham Sourav Brahma <subham.sourav.brahma@sap.com>
2024-05-09 23:54:12 +0530

    Initial Commit

---
+++ src/main/java/com/embarkx/firstjobapp/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,69 @@
+package com.embarkx.firstjobapp.company.impl;
+
+import com.embarkx.firstjobapp.company.Company;
+import com.embarkx.firstjobapp.company.CompanyRepository;
+import com.embarkx.firstjobapp.company.CompanyService;
+import com.embarkx.firstjobapp.job.Job;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company company, Long id) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (companyOptional.isPresent()) {
+            Company companyToUpdate = companyOptional.get();
+    
+            if (company.getName() != null) {
+                companyToUpdate.setName(company.getName());
+            }
+    
+            if (company.getDescription() != null) {
+                companyToUpdate.setDescription(company.getDescription());
+            }
+    
+            if (company.getJobs() != null) {
+                companyToUpdate.setJobs(company.getJobs());
+            }
+    
+            companyRepository.save(companyToUpdate);
+            return true;
+        }
+        return false;
+    }
+    
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public boolean deleteCompany(Long id) {
+        if(companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+}
